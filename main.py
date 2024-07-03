cd2b27c378c45880c0d818c3fd4bfed372d5b698
James Anderson <J.anderson@penspen.com>
2024-07-03 09:23:16 +0100

    Feat: First iteration of the addition function.

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 keep_going = True
 count = 0
-full_number = 1
+full_number = 0
 while keep_going:
-    count += 1
     if_number = False
     while if_number == False:
-        newnumber = input("Enter a number you would like to multiply: ")
+        newnumber = input("Enter a number you would like to add: ")
         if newnumber.isdigit():
             newnumber = int(newnumber)
             if_number = True
         else:
             print("This needs to be an integer! (E.g. 10)")
-    full_number *= newnumber
+    full_number += newnumber
     if count == 0:
+        count += 1
         pass
     else:
         while  True:
-            print("Your current total is: ",full_number,"\n Would you like to multiply it by another number? (Yes or no)")
+            print("Your current total is: ",full_number,"\n Would you like to add another number? (Yes or no)")
             confirm = input()
             if confirm.lower() == "yes":
+                count += 1
                 break
             elif confirm.lower() == "no":
                 keep_going = False
                 break
             else: 
                 print("Incorrect input - the only accepted inputs are yes and no!")
+            count += 1
