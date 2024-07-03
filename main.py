c99f42d8faa2b835948a9a5e95a754c64287c26e
James Anderson <J.anderson@penspen.com>
2024-07-03 10:20:41 +0100

    Feat: multiplacation iteration 2- First was deleted?

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-keep_going = True
-count = 0
 full_number = 1
-while keep_going:
-    count += 1
-    if_number = False
-    while if_number == False:
-        newnumber = input("Enter a number you would like to multiply: ")
-        if newnumber.isdigit():
-            newnumber = int(newnumber)
-            if_number = True
+another_number = True
+while another_number:
+    number_good = False
+    while not number_good:
+        new_number = input("Please enter the number you would like to multiply by: ")
+        if new_number.isdigit():
+            new_number = int(new_number)
+            number_good = True
         else:
-            print("This needs to be an integer! (E.g. 10)")
-    full_number *= newnumber
-    if count == 0:
-        pass
-    else:
-        while  True:
-            print("Your current total is: ",full_number,"\n Would you like to multiply it by another number? (Yes or no)")
-            confirm = input()
-            if confirm.lower() == "yes":
-                break
-            elif confirm.lower() == "no":
-                keep_going = False
-                break
-            else: 
-                print("Incorrect input - the only accepted inputs are yes and no!")
+            print("The number must be an integer!")
+    full_number *= new_number
+    print("Your full multiplication is:", full_number)
+    
+    accepted_input = False
+    while not accepted_input:
+        keep_going = input("Would you like to multiply by another number? (yes or no) ")
+        if keep_going.lower() == "yes":
+            accepted_input = True
+        elif keep_going.lower() == "no":
+            another_number = False
+            accepted_input = True
+        else:
+            print("This is not an accepted input. Please input 'yes' or 'no'.")
+
+
+
